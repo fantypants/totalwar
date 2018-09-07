e508fe4e0032b110c78dcf78622f308a7b7e182b
fantypants <8563615@gmail.com>
2018-09-07 12:01:40 -0700

    single and double horizontal placements created

--- lib/totalwar/services/placements.ex
+++ lib/totalwar/services/placements.ex
@@ -4,34 +4,83 @@ alias Totalwar.Services.Frigate
 def get_placements(position, ship) do
   case ship do
     "Frigate" ->
-      calculate_position_horizontal(position, %Totalwar.Services.Frigate{})
-    "Aircraft Carrier" -> %Totalwar.Services.AircraftCarrier{}
-    "Destoryer" -> %Totalwar.Services.Destroyer{}
-    "Patrol" -> %Totalwar.Services.Patrol{}
-    "Submarine" -> %Totalwar.Services.Submarine{}
-    "Tactical Boat" -> %Totalwar.Services.TacticalBoat{}
+      calculate_position(position, %Totalwar.Services.Frigate{}, "horizontal")
+    "Aircraft Carrier" ->
+      calculate_position(position, %Totalwar.Services.AircraftCarrier{}, "horizontal")
+    "Destroyer" ->
+      calculate_position(position, %Totalwar.Services.Destroyer{}, "horizontal")
+    "Patrol" ->
+      calculate_position(position, %Totalwar.Services.Patrol{}, "horizontal")
+    "Submarine" ->
+      calculate_position(position, %Totalwar.Services.Submarine{}, "horizontal")
+    "Tactical Boat" ->
+      calculate_position(position, %Totalwar.Services.TacticalBoat{}, "horizontal")
   end
 end
 
-defp calculate_position_horizontal(position, struct) do
+defp calculate_position(position, struct, type) do
   length = struct.length
   width = struct.width
-  x_start = position.x
-  y_start = position.y
-  x_end = (position.x + length) -position.x
-  y_end = position.y
-  positions_x = Enum.to_list(x_start..x_end)
-  positions_map = positions_x
-    |> Enum.map(fn(x) ->
-        %{x: x, y: y_end}
-      end) |> IO.inspect
-  {:ok, positions_map}
+  case type do
+    "horizontal" ->
+      case width do
+        1 ->
+          single_position(position, length, width, "horizontal")
+        2 ->
+          double_position(position, length, width, "horizontal")
+        _->
+        :error
+      end
+    "vertical" ->
+      case width do
+        1 ->
+          single_position(position, length, width, "vertical")
+        2 ->
+          double_position(position, length, width, "vertical")
+        _->
+        :error
+      end
+  end
 end
 
-defp convert_to_atom_NOT_USED(positions, integer, operator) do
-index_position = Integer.to_string(Enum.find_index(positions, fn(position) -> position == integer end))
-String.to_atom(operator <> index_position)
+defp single_position(position, length, width, type) do
+  case type do
+    "vertical" ->
+      positions_x = Enum.to_list(position.x..((position.x + length) -position.x))
+      positions_map = create_position_map(positions_x, position, "single")
+      {:ok, positions_map}
+    "horizontal" ->
+      positions_x = Enum.to_list(position.x..((position.x + length) -position.x))
+      positions_map = create_position_map(positions_x, position, "single")
+      {:ok, positions_map}
+  end
 end
+
+defp double_position(position, length, width, type) do
+  case type  do
+    "horizontal" ->
+      positions_x = Enum.to_list(position.x..((position.x + length) -position.x))
+      positions_map_1 = create_position_map(positions_x, position, "single")
+      positions_map_2 = create_position_map(positions_x, position, "double")
+      {:ok, positions_map_1 ++ positions_map_2}
+    "vertical" ->
+      positions_x = Enum.to_list(position.x..((position.x + length) -position.x))
+      positions_map_1 = create_position_map(positions_x, position, "single")
+      positions_map_2 = create_position_map(positions_x, position, "double")
+      {:ok, positions_map_1 ++ positions_map_2}
+  end
+end
+
+defp create_position_map(positions_x, position, type) do
+  case type do
+    "single" ->
+      positions_x |> Enum.map(fn(x) -> %{x: x, y: position.y} end)
+    "double" ->
+      positions_x |> Enum.map(fn(x) -> %{x: x, y: position.y + 1} end)
+  end
+end
+
+
 
 
 
--- test/totalwar/services/placements.exs
+++ test/totalwar/services/placements.exs
@@ -2,8 +2,33 @@ defmodule Totalwar.Services.PlacementsTest do
   use TotalwarWeb.ConnCase
   alias Totalwar.Services.Placements
 
-  test "ship recieves correct horizontal placement params" do
+  test "ship recieves correct horizontal single placement params" do
     assert Placements.get_placements(%{x: 1, y: 1}, "Frigate") == {:ok, [%{x: 1, y: 1}, %{x: 2, y: 1}, %{x: 3, y: 1}]}
+  end
+
+  test "ship recieves correct horizontal double placement params" do
+    assert Placements.get_placements(%{x: 1, y: 1}, "Aircraft Carrier") == {:ok, [
+      %{x: 1, y: 1},
+      %{x: 2, y: 1},
+      %{x: 3, y: 1},
+      %{x: 4, y: 1},
+      %{x: 5, y: 1},
+      %{x: 6, y: 1},
+      %{x: 7, y: 1},
+      %{x: 8, y: 1},
+      %{x: 1, y: 2},
+      %{x: 2, y: 2},
+      %{x: 3, y: 2},
+      %{x: 4, y: 2},
+      %{x: 5, y: 2},
+      %{x: 6, y: 2},
+      %{x: 7, y: 2},
+      %{x: 8, y: 2}
+    ]}
+  end
+
+  test "ship recieves correct horizontal single placement params destroyer" do
+    assert Placements.get_placements(%{x: 1, y: 1}, "Destroyer") == {:ok, [%{x: 1, y: 1}, %{x: 2, y: 1}, %{x: 3, y: 1}, %{x: 4, y: 1}]}
   end
 
 end
